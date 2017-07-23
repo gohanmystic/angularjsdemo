e8bbbe5aaa432bf784b7b823a345e60c705788cc
gohanmystic <vutran26999@gmail.com>
2017-07-24 00:30:21 +0700

    demo

---
+++ audits/audits.state.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('crmApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('audits', {
+            parent: 'admin',
+            url: '/audits',
+            data: {
+                authorities: ['ADMIN'],
+                pageTitle: 'Audits'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/admin/audits/audits.html',
+                    controller: 'AuditsController',
+                    controllerAs: 'vm'
+                }
+            }
+        });
+    }
+})();
